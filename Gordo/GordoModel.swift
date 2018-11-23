81281ad6b6ddf7f0096d83b82f0d63216cacecb1
RodrigoChousal <rodrigochousal@gmail.com>
2018-11-23 13:17:15 -0600

    Started data model

---
+++ Gordo/GordoModel.swift
@@ -0,0 +1,16 @@
+//
+//  GordoModel.swift
+//  Gordo
+//
+//  Created by Rodrigo Chousal on 11/23/18.
+//  Copyright © 2018 Rodrigo Chousal. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import UIKit
+
+class Food {
+	var length: Int
+	var sprite: UIImage
+}
